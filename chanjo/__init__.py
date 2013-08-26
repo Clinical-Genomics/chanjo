16755bc5eaab46c1c13be37b548698bdb99164c7
Robin Andeer <robin.andeer@gmail.com>
2013-08-26 15:03:28 +0200

    Source code annotations "complete"
    Former-commit-id: 84491aae4448a19c07e3bb056a47ac0395995415

--- chanjo/__init__.py
+++ chanjo/__init__.py
@@ -1,4 +1,9 @@
 from __future__ import print_function
 
-__copyright__ = "MIT Copyright (c) 2013 Robin Andeer"
-__version__ = "0.0.1"
+
+__title__ = 'chanjo'
+__version__ = '0.0.1'
+__author__ = 'Robin Andeer'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 Robin Andeer'
+__docformat__ = 'restructuredtext'
