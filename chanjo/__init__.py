b6e445ebea0ceb5d550867befb3350c44e6812be
Robin Andeer <robin.andeer@gmail.com>
2013-08-27 12:04:13 +0200

    removed api
    Former-commit-id: cc14f223637468e70479f7f0b20e6bb41794667f

--- chanjo/__init__.py
+++ chanjo/__init__.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 
 __title__ = 'chanjo'
-__version__ = '0.0.1'
+__version__ = '0.0.1-beta'
 __author__ = 'Robin Andeer'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2013 Robin Andeer'
