4bb1720e568f640decbf8713ee08056e8b8c771c
Robin Andeer <robin.andeer@gmail.com>
2013-08-27 10:26:56 +0200

    update to setup
    Former-commit-id: 1c49197173e44e7511bc0c8713b5dc2428a84404

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import chanjo
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-config = {
-    "description": "A coverage analysis package for clinical exome"
-                   "sequencing.",
-    "author": "Robin Andeer",
-    "author_email": "robin.andeer@gmail.com",
-    "url": "http://chanjo.herokuapp.com/",
-    "download_url": "https://github.com/robinandeer/chanjo2",
-    "version": "0.1",
-    "install_requires": [
+setup(
+    name="chanjo",
+    description="A coverage analysis package for clinical sequencing.",
+    author="Robin Andeer",
+    author_email="robin.andeer@gmail.com",
+    url="http://chanjo.herokuapp.com/",
+    download_url="https://github.com/robinandeer/chanjo",
+    version=chanjo.__version__,
+    install_requires=[
         "nose",
         "pysam",
-        "autumn",
+        "numpy",
+        "sqlalchemy",
         "interval",
-        "bx-python"
+        "bx-python",
+        "docopt"
     ],
-    "packages": ["chanjo"],
-    "scripts": [],
-    "name": "chanjo"
-}
-
-setup(**config)
+    packages=["chanjo"],
+    scripts=["scripts/chanjo-autopilot.py"]
+)
