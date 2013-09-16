c077645dddcb03da08e3b7ee887dd262e5d6e4d8
Robin Andeer <robin.andeer@gmail.com>
2013-09-16 10:57:36 +0200

    corrected column type

--- chanjo/sql.py
+++ chanjo/sql.py
@@ -467,7 +467,7 @@ class Exon(Base):
   end = sql.Column(sql.Integer)
   strand = sql.Column(sql.String)
 
-  levels = sql.Column(sql.String)
+  levels = sql.Column(sql.BLOB)
   coverage = sql.Column(sql.Float)
   completeness = sql.Column(sql.Float)
   cutoff = sql.Column(sql.Integer)
