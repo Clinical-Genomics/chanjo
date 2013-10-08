935c12b24cf90d2eba825d9d5d4e441915ab5d73
Robin Andeer <robin.andeer@gmail.com>
2013-10-08 11:30:20 +0200

    - Removed deprecated tests
    Former-commit-id: a2d6a6022cf40e345ce7fa3399cad254eccf74e5

--- tests/sql_tests.py
+++ tests/sql_tests.py
@@ -13,17 +13,6 @@ class TestCoverageAdapter:
 
   def setUp(self):
     print "SETUP!"
-    # Set up all tables
-    self.adapter.setup()
-    # Create a single gene object, add and commit to database
-    gene = self.adapter.create("gene", hgnc="GIT1", chrom="17", start=12345,
-                               end=14641, strand="+")
-    self.adapter.add(gene)
-    self.adapter.commit()
 
   def tearDown(self):
     print "TEAR DOWN!"
-
-  def test_get(self):
-    gene = self.adapter.get("gene", "GIT1")
-    assert_equal(gene.chrom, "17")
