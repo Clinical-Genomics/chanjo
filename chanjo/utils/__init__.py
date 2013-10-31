e8ed91260142de5c798770c56f110ac6e853920d
Robin Andeer <robin.andeer@gmail.com>
2013-10-31 11:13:24 +0100

    NEW chromosome function to build a list of chromosome IDs CHANGED intervals now return surplus columns CHANGED fileCheck now optionally checks multiple files Cleaned up script CLI to adapt to new architecture
    Former-commit-id: 54cce68909361332db8596b0b91485e1edb063ac

--- chanjo/utils/__init__.py
+++ chanjo/utils/__init__.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+def chromosomes(prepend="", extra=[]):
+  # Stringify
+  base = map(str, range(1,23))
+  return [prepend + chrom for chrom in base + ["X", "Y"] + extra]
