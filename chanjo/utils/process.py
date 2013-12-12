4d9bbec758f43cdd7f5ba18011bf80b689a20bb6
Robin Andeer <robin.andeer@gmail.com>
2013-12-12 10:59:41 +0100

    FIXES silly method/function error in “process” CHANGES “process” function and adds inline docs

--- chanjo/utils/process.py
+++ chanjo/utils/process.py
@@ -2,27 +2,33 @@
 # coding: utf-8
 
 import itertools
-import numpy as np
 
-from calculate import intervals
-from group import group
+from .calculate import intervals
+from .group import group
 
 
-def process(self, bamFile, chrom, segments,
-            extend=0, cutoff=10, threshold=10000):
+def process(bamFile, chrom, segments, extend=0, cutoff=10, threshold=10000):
   """
   <public> Annotates each related exon with coverage data.
 
   .. code-block:: python
+    >>> from chanjo import utils, bam
+    >>> bamFile = bam.CoverageAdaper("myBamFile.bam")
+    >>> exons = [(1, 10, "exon1"), (15, 30, "exon2"), ...]
+    >>> data = utils.process(bamFile, 1, exons, 0, 10, 50)
 
-  :param object element: One element object (gene/transcript)
-  :param int cutoff: (optional) Min read depth for completeness [default: 10]
-  :param bool splice: (optional) Include splice sites for each exon (+/- 2)
+  :param object bamFile: Initialized CoverageAdaper object
+  :param str chrom: Chromosome ID
+  :param list segments: List of continous genomic segments
+  :param int extend: How much to extend each segment with [default: 0]
+  :param int cutoff: Min read depth for completeness [default: 10]
+  :param bool threshold: Target length for grouping segments [default: 10000]
   """
   groups = group(segments, threshold=threshold, extend=extend)
-  data = np.zeros(len(groups))
+  list_groups = list(groups)
+  data = [0]*len(list_groups)
 
-  for i, group_ in enumerate(groups):
+  for i, group_ in enumerate(list_groups):
     
     read_depth = bamFile.read(chrom, group_[0][0], group_[-1][1])
     data[i] = intervals(group_, read_depth, cutoff=cutoff)
