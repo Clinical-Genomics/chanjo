033dc56046ba189f601f316a887f23a61615d772
Robin Andeer <robin.andeer@gmail.com>
2014-05-04 08:57:44 +0200

    Sets up travis to use conda build Adds default ccds converter as dependency Updates setup.py to use renamed readme

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.argv[-1] == 'publish':
   os.system('python setup.py sdist upload')
   sys.exit()
 
-with open('README.rst') as f:
+with open('README.md') as f:
   readme = f.read()
 
 with open('LICENSE') as f:
