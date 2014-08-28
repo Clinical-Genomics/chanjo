ffeffaf06a4c2a4a64587752bbe84904667eaced
Robin Andeer <robin.andeer@gmail.com>
2014-08-28 19:51:33 +0200

    Reorder code

--- chanjo/config/cli.py
+++ chanjo/config/cli.py
@@ -7,24 +7,6 @@ from .. import __banner__
 
 
 @click.command()
-@click.pass_context
-def init(context):
-  """Walk user through setting up a new config file."""
-  # print a nice welcome message
-  click.echo(__banner__)
-
-  questions = [
-    ('annotate.cutoff', 'sufficient coverage',
-      context.obj.get('annotate', {}).get('cutoff', 10)),
-    ('dialect', 'preferred SQL-dialect', context.parent.db.dialect),
-    ('db', 'central database path/URI', context.parent.db.uri)
-  ]
-
-  # launch init pipeline
-  init_pipeline(__package__, context.obj, questions)
-
-
-@click.command()
 @click.option('--remove', is_flag=True, help='remove config option')
 @click.argument('key')
 @click.argument('value', required=False)
@@ -47,3 +29,21 @@ def config(context, key, value, remove):
 
   # persist updates to the config file
   context.obj.save()
+
+
+@click.command()
+@click.pass_context
+def init(context):
+  """Walk user through setting up a new config file."""
+  # print a nice welcome message
+  click.echo(__banner__)
+
+  questions = [
+    ('annotate.cutoff', 'sufficient coverage',
+      context.obj.get('annotate', {}).get('cutoff', 10)),
+    ('dialect', 'preferred SQL-dialect', context.parent.db.dialect),
+    ('db', 'central database path/URI', context.parent.db.uri)
+  ]
+
+  # launch init pipeline
+  init_pipeline(__package__, context.obj, questions)
