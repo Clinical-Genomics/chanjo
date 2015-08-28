5da4124fddf3b0b3abd705cc8a83dc2a40804644
moonso <monsunas@gmail.com>
2015-08-28 08:41:27 +0200

    Updated log.py

--- chanjo/log.py
+++ chanjo/log.py
@@ -1,35 +1,59 @@
-# -*- coding: utf-8 -*-
+import sys
 import logging
 
-from . import __title__
-
-logger = logging.getLogger(__title__)
-logger.setLevel(logging.DEBUG)
-
-LEVELS = {0: logging.ERROR, 1: logging.WARN, 2: logging.INFO,
-          3: logging.DEBUG}
-
-
-def make_handler(stream, level=logging.INFO):
-    """Setup a logging handler connected to the logger.
-
-    Args:
-        stream (file): file-like object with ``write`` + ``flush`` methods
-        level (level, optional): lower logger level to accept
-
-    Returns:
-        handler: configured logging ``StreamHandler`` instance
+def init_log(logger, filename=None, loglevel=None):
     """
-    # create stream/file handler
-    handler = logging.StreamHandler(stream)
-    handler.setLevel(level)
+    Initializes the log file in the proper format.
 
-    # set formatter
-    template = "%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s"
+    Arguments:
+
+        filename (str): Path to a file. Or None if logging is to
+                         be disabled.
+        loglevel (str): Determines the level of the log output.
+    """
+    template = '[%(asctime)s] %(levelname)-8s: %(name)-25s: %(message)s'
     formatter = logging.Formatter(template)
-    handler.setFormatter(formatter)
 
-    # add handler to logger
-    logger.addHandler(handler)
+    if loglevel:
+        logger.setLevel(getattr(logging, loglevel))
 
-    return handler
+    # We will always print warnings and higher to stderr
+    console = logging.StreamHandler()
+    console.setLevel('WARNING')
+    console.setFormatter(formatter)
+
+    if filename:
+        file_handler = logging.FileHandler(filename, encoding='utf-8')
+        if loglevel:
+            file_handler.setLevel(getattr(logging, loglevel))
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    # If no logfile is provided we print all log messages that the user has
+    # defined to stderr
+    else:
+        if loglevel:
+            console.setLevel(getattr(logging, loglevel))
+
+    logger.addHandler(console)
+
+def get_log_stream(logger):
+    """
+    Returns a stream to the root log file.
+    If there is no logfile return the stderr log stream
+    
+    Returns:
+        A stream to the root log file or stderr stream.
+    """
+    
+    file_stream = None
+    log_stream = None
+    for handler in logger.handlers:
+        if isinstance(handler, logging.FileHandler):
+            file_stream = handler.stream
+        else:
+            log_stream = handler.stream
+    
+    if file_stream:
+        return file_stream
+    
+    return log_stream
