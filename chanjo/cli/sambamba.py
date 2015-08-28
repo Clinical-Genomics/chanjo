f54f5f0ef462c34df4f9c3e11aca6968573be125
moonso <monsunas@gmail.com>
2015-08-28 08:38:43 +0200

    Added logging to sambamba.py

--- chanjo/cli/sambamba.py
+++ chanjo/cli/sambamba.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import click
-
+import logging
 import subprocess
 
 @click.command()
@@ -9,11 +9,26 @@ import subprocess
 )
 @click.option('-e', '--exon_bed',
                 type=click.Path(exists=True),
+                help="Path to a bed file with exon coordinates"
+)
+@click.option('-g', '--gene_bed',
+                type=click.Path(exists=True),
+                help="Path to a bed file with gene coordinates"
+)
+@click.option('-t', '--cov_treshold',
+                multiple=True,
+                help="multiple thresholds can be provided,"\
+                     "for each one an extra column will be added,"\
+                     "the percentage of bases in the region"\
+                     "where coverage is more than this value"
 )
 @click.pass_context
-def sambamba(context, bam_file, exon_bed):
+def sambamba(context, bam_file, exon_bed, gene_bed, cov_treshold):
     """Run Sambamba from chanjo."""
-    click.echo("bam_file:{0}".format(bam_file))
+    logger = logging.getLogger(__name__)
+    #For testing only:
+    logger = logging.getLogger("chanjo.cli.sambamba")
+    logger.info("bam_file:{0}".format(bam_file))
 
 
 def run_sambamba():
@@ -22,4 +37,5 @@ def run_sambamba():
 
 
 if __name__ == '__main__':
+    # from chanjo.log import make_handler
     sambamba()
