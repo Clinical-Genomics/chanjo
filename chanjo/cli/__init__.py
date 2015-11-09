417c10542888873e785a619b1ff662a98b68387e
Robin Andeer <robin.andeer@gmail.com>
2015-11-09 15:07:58 +0100

    add sex guess command, fix #158

--- chanjo/cli/__init__.py
+++ chanjo/cli/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from .bootstrap import bootstrap as bootstrap_command
 from .calculate import calculate as calculate_command
 from .database import db as database_command
 from .init import init as init_command
@@ -6,4 +7,4 @@ from .link import link as link_command
 from .load import load as load_command
 from .root import root as root_command
 from .sambamba import sambamba as sambamba_command
-from .bootstrap import bootstrap as bootstrap_command
+from .sex import sex as sex_command
