52f001058f7e16fb68589cab8099d66ef56c11e2
northwestwitch <rasi.chiara@gmail.com>
2018-03-06 14:28:46 +0100

    fix #191 : No module named request

--- chanjo/init/bootstrap.py
+++ chanjo/init/bootstrap.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import zipfile
-import urllib.request
+import urllib
 
 from path import Path
 
@@ -28,7 +28,7 @@ def pull(target_dir, force=False):  # pragma: no cover
 
     if not final_bed.exists() or force:
         logger.info('downloading... [%s]', BED_URL)
-        urllib.request.urlretrieve(BED_URL, bed_zip_path)
+        urllib.urlretrieve(BED_URL, bed_zip_path)
 
         logger.info('extracting BED file...')
         zip_ref = zipfile.ZipFile(bed_zip_path, 'r')
