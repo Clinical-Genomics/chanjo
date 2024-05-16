4126ae8e3cbe431e04b4cdd0d168e8c4a6bf808d
Chiara Rasi <rasi.chiara@gmacil.com>
2024-05-16 16:28:13 +0200

    Work in progress

--- tests/store/test_store_api.py
+++ tests/store/test_store_api.py
@@ -25,27 +25,27 @@ def test_no_dialect():
 def test_save(chanjo_db):
     # GIVEN a new sample
     sample_id = "ADM12"
-    new_sample = Sample(id=sample_id, group_id="ADMG1", source="alignment.bam")
-    # WHEN added and saved to the database
-    chanjo_db.add(new_sample)
-    chanjo_db.save()
-    # THEN is should exist in the database
-    assert new_sample.id == sample_id
-    assert isinstance(new_sample.created_at, datetime)
-    assert Sample.query.get(sample_id) == new_sample
+    with chanjo_db.begin() as session:
+        new_sample = Sample(id=sample_id, group_id="ADMG1", source="alignment.bam")
+        # WHEN added and saved to the database
+        session.add(new_sample)
+        # THEN is should exist in the database
+        assert new_sample.id == sample_id
+        assert isinstance(new_sample.created_at, datetime)
+        assert Sample.query.get(sample_id) == new_sample
 
-    # GIVEN sample already exists
-    conflict_sample = Sample(id=sample_id, group_id="ADMG2")
-    # WHEN saving it again with same id
-    # THEN error is raised _after_ rollback
-    with pytest.raises(FlushError):
-        chanjo_db.add(conflict_sample)
+        # GIVEN sample already exists
+        conflict_sample = Sample(id=sample_id, group_id="ADMG2")
+        # WHEN saving it again with same id
+        # THEN error is raised _after_ rollback
+        with pytest.raises(FlushError):
+            chanjo_db.add(conflict_sample)
+            chanjo_db.save()
+
+        new_sampleid = "ADM13"
+        chanjo_db.add(Sample(id=new_sampleid))
         chanjo_db.save()
-
-    new_sampleid = "ADM13"
-    chanjo_db.add(Sample(id=new_sampleid))
-    chanjo_db.save()
-    assert Sample.query.get(new_sampleid)
+        assert Sample.query.get(new_sampleid)
 
 
 def test_add_many(chanjo_db):
