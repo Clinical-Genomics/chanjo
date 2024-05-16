76ce1ed4789ef1ece90bdc704d8073e118a9f979
Chiara Rasi <rasi.chiara@gmacil.com>
2024-05-16 15:28:00 +0200

    Fixed another test

--- tests/cli/test_cli_load.py
+++ tests/cli/test_cli_load.py
@@ -17,13 +17,14 @@ def test_load_conflict(popexist_db, invoke_cli, sambamba_path):
     # GIVEN an existing database with a sample
     db_uri = popexist_db.uri
     sample_id = 'sample'
-    assert Sample.query.first().id == sample_id
-    # WHEN you try to add the same sample id twice
-    result = invoke_cli(['--database', db_uri, 'load', '--sample', sample_id,
-                         sambamba_path])
-    # THEN it should fail
-    assert result.exit_code != 0
-    assert Sample.query.count() == 1
+    with popexist_db.begin() as session:
+        assert session.first(Sample.select()).id == sample_id
+        # WHEN you try to add the same sample id twice
+        result = invoke_cli(['--database', db_uri, 'load', '--sample', sample_id,
+                             sambamba_path])
+        # THEN it should fail
+        assert result.exit_code != 0
+        assert len(session.all(Sample.select())) == 1
 
 
 def test_link(existing_db, invoke_cli, bed_path):
