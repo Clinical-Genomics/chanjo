c1f7daa898d6132a97a6aa5e040d2b9805ef6a34
Chiara Rasi <rasi.chiara@gmacil.com>
2025-12-02 15:24:49 +0100

    Fix other test

--- tests/init/test_init_bootstrap.py
+++ tests/init/test_init_bootstrap.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
+import pytest
 from mock import patch
 
 from chanjo.init import bootstrap
 from chanjo.testutils import FakeZipFile, fake_urlretrieve
 
 
+@pytest.mark.parametrize("build", ["37", "38"])
 @patch("urllib.request.urlretrieve", fake_urlretrieve)
 @patch("zipfile.ZipFile", FakeZipFile)
-def test_pull(tmp_path):
-    # GIVEN a target directory
+def test_pull(tmp_path, build):
     target_dir = str(tmp_path)
+
     # WHEN downloading resources
-    bootstrap.pull(target_dir)
+    bootstrap.pull(target_dir, build=build)
+
     # THEN BED resource should be in place
-    out_bed = tmp_path.joinpath(bootstrap.BED_NAME)
-    assert out_bed.exists()
+    bed_file = tmp_path.joinpath(bootstrap.BED_NAME[build])
+    assert bed_file.exists()
+
+    # Check that only this file is in the directory
     assert len(list(tmp_path.iterdir())) == 1
 
-    # GIVEN the resources already exists
-    bootstrap.pull(target_dir)
-    # THEN nothing happens :)
+    # GIVEN the resources already exist
+    bootstrap.pull(target_dir, build=build)
+    # THEN nothing happens (file already exists)
+    assert bed_file.exists()
